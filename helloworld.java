954f3a54a789cae56e2d32c01ff463b3cf827328
Sakshi26400 <72200070+Sakshi26400@users.noreply.github.com>
2020-10-01 16:36:32 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,9 @@
-public class helloworld{
-    public static void main(String[] arg){
-         System.out.println("Hello " + " World");
+//A program to print hello world
+public class helloworld    //creating class
+{
+    public static void main(String[] arg)
+    {
+         //System.out.println("Hello " + " World");     //example of string concatnation
+        System.out.println("Hello, World!");    //printing string without concatnation 
     }
 }
